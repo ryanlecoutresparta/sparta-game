39292f37fb41944d6ce29f9b2c218de9cd9b6862
Tech-A07 <tech-a07@Tech-A07s-MacBook-Air.local>
2018-09-20 17:01:13 +0100

    attempting to cache scores and completed high scores page

--- js/scores.js
+++ js/scores.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-let scoreGot = document.cookie;
-console.log(scoreGot)
-scoreGot.setAttribute('class', 'currentScore');
+
+
+function readCookie() {
+  let allcookies = document.cookie;
+  document.write (allcookies);
+}
+
+// let scoreGot = document.cookie;
+// console.log(scoreGot)
+// scoreGot.setAttribute('class', 'currentScore');
 
 })
