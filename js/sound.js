88a5a97c393d23aed226413df31c74a85d2da6e7
Tech-A07 <rlecoutre@spartaglobal.com>
2018-10-28 20:49:37 +0000

    additional errors fixed

--- js/sound.js
+++
@@ -1,9 +0,0 @@
-// const Game = new Object();
-//
-// Game.gameOverAudio1 = document.getElementById('iFrameAudio');
-// 
-// let isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-//
-// if (!isChrome) {
-//   Game.gameOverAudio1.remove()
-// }
